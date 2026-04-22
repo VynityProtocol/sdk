ae088b3b98185f2a994ccb2904ceae8f65adcd86
Vynity <susimantia09@gmail.com>
2026-04-22 23:57:49 +0700

    Create Navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#111", color: "#fff" }}>
+      <h2>Vynity</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
